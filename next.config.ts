b37bdf00acb245d6b852452dec2ddf56da4b9410
Khangtong <duykhangtong99@gmail.com>
2024-11-03 11:09:11 +0700

    Complete

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // Partial Prerendering
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
 };
 
 export default nextConfig;
